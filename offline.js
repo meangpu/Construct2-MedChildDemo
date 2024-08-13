f3cb83535ff526dc3e13b69d381c3d7cd7641e4e
meangpu <51825872+meangpu@users.noreply.github.com>
2024-08-13 23:03:02 +0700

    update web

--- offline.js
+++ offline.js
@@ -1,9 +1,23 @@
 ﻿{
-	"version": 1723562169,
+	"version": 1723564928,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
-		"offlineClient.js"
+		"offlineClient.js",
+		"images/map.png",
+		"images/chooselevel-sheet0.png",
+		"images/level1-sheet0.png",
+		"images/choice-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/tickclassicicon-sheet0.png",
+		"images/correct-sheet0.png",
+		"images/wrong-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
 	]
 }
